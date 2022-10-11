8caa2372cb43c9da64bebd8115b7b6bcbed318e3
jupanaos <julie.pariona@gmail.com>
2022-10-12 00:18:12 +0200

    feat: #8 created register+routes

---
+++ src/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends AbstractController
+{
+    #Route('/register', name: 'register')
+    public function showRegister(): Response
+    {
+        return $this->render('client/register.html.twig');
+    }
+}
